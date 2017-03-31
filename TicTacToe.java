a22e3b1df3758982c4d82ef294d97c8c7227a5e4
Miciaha Ivey <mdi13b@my.fsu.edu>
2017-03-31 16:24:14 -0400

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoegame;
 import java.util.Scanner;
 import java.util.Random;
-/**
- *
- * @author Miciaha
- */
+
 public class TicTacToe {
     
         //Initializing values
